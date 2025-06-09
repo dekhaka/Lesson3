1179c885c36e01a094766d0521cdb4196af2700c
Edward <vikuloveduard5@gmail.com>
2025-06-09 14:46:48 +0300

    KS-3-1 A greeting has been added and displayed in the console

---
+++ src/main/kotlin/Lesson_3/lesson_3_task_1.kt
@@ -0,0 +1,11 @@
+fun main() {
+
+    var sayHello = "Добрый день"
+    val nameVisiter = "Эдуард"
+
+    println("$sayHello, $nameVisiter!")
+
+    sayHello = "Добрый вечер"
+    println("$sayHello, $nameVisiter!")
+
+}
