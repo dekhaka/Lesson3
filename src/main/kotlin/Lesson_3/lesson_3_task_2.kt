558614ae2bbd7c3eebd2a3ef86287a8f6ce0d9de
Edward <vikuloveduard5@gmail.com>
2025-06-09 14:54:36 +0300

    KS-3-2 Variables have been added and output to the console

---
+++ src/main/kotlin/Lesson_3/lesson_3_task_2.kt
@@ -0,0 +1,15 @@
+fun main() {
+
+    var surname = "Андреева"
+    val name = "Татьяна"
+    val patronymic = "Сергеевна"
+    var age = 20
+
+    println("$surname $name $patronymic, $age лет.")
+
+    surname = "Сидорова"
+    age = 22
+
+    println("$surname $name $patronymic, $age года.")
+
+}
