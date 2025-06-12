ff28470bd8738cd0521aa5e8a81eaea05c1c2432
Edward <vikuloveduard5@gmail.com>
2025-06-12 22:51:44 +0300

    KS-3-3 исправлены замечания

--- src/main/kotlin/Lesson_3/lesson_3_task_3.kt
+++ src/main/kotlin/Lesson_3/lesson_3_task_3.kt
@@ -3,15 +3,15 @@ fun main() {
     val variable = 4
 
     val multiplicationTable = """
-        $variable x 1 = ${variable*1}
-        $variable x 2 = ${variable*2}
-        $variable x 3 = ${variable*3}
-        $variable x 4 = ${variable*4}
-        $variable x 5 = ${variable*5}
-        $variable x 6 = ${variable*6}
-        $variable x 7 = ${variable*7}
-        $variable x 8 = ${variable*8}
-        $variable x 9 = ${variable*9}
+        $variable x 1 = ${variable * 1}
+        $variable x 2 = ${variable * 2}
+        $variable x 3 = ${variable * 3}
+        $variable x 4 = ${variable * 4}
+        $variable x 5 = ${variable * 5}
+        $variable x 6 = ${variable * 6}
+        $variable x 7 = ${variable * 7}
+        $variable x 8 = ${variable * 8}
+        $variable x 9 = ${variable * 9}
         
     """.trimIndent()
 
