beb4c623f2386c7db33e2bdd66b1b416fae44496
Edward <vikuloveduard5@gmail.com>
2025-06-12 23:03:45 +0300

    KS-3-5 исправлены замечания

--- src/main/kotlin/Lesson_3/lesson_3_task_5.kt
+++ src/main/kotlin/Lesson_3/lesson_3_task_5.kt
@@ -2,13 +2,10 @@ fun main() {
 
     val moveString = "D2-D4;0"
 
-    val parts = moveString.split(";")
-    val moveCoordinats = parts[0]
-    val moveNumber = parts[1].toInt()
-
-    val coords = moveCoordinats.split("-")
-    val from = coords[0]
-    val to = coords[1]
+    val parts = moveString.split("-",";")
+    val from = parts [0]
+    val to = parts [1]
+    val moveNumber = parts[2].toInt()
 
     println("Откуда: $from")
     println("Куда: $to")
